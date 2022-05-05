0288911aecbbb215666d990172fd514369908972
Ayodele Damilola Yetunde <72695974+oluwadamilola21@users.noreply.github.com>
2022-05-05 21:58:18 +0100

    Delete Sansbrother directory

--- Sansbrother/app.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector(".form-check");
-const check = document.querySelector(".form-check-label");
-list.addEventListener('click', function() {
-    if(check.classList.contains('checked')) {
-        check.classList.remove('checked')
-    } else {
-        check.classList.add('checked')
-    }
-  });
